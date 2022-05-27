a1161b24c0fa27b941bb292f37a6e28ec38b80a5
Vovk <vovk.valki@ukr.net>
2022-05-27 16:06:37 +0300

    add page ABOUT.HTML and INDEX>HTML
    add to directory templates/mainapp two file,
    Crete dynamic section with JINJA,
    Create sample layout.html

--- suite/mainapp/views.py
+++ suite/mainapp/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("<h4> проверка работы mainapp/view.py при обращении к главной страницы</h4>")
+    return render(request, 'mainapp/index.html')
 
 def about(request):
-    return HttpResponse("<h5>Страница про нас</h5>")
+    return render(request, 'mainapp/about.html')
