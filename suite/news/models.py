92bbc2bf1d643170d701020c800fe076c393f215
Vovk <vovk.valki@ukr.net>
2022-06-04 15:49:31 +0300

    add model "motors"
    write it to database
    install Pillow for work with ImageField

--- suite/news/models.py
+++ suite/news/models.py
@@ -14,3 +14,15 @@ class Artiles(models.Model):
         verbose_name = 'New'
         verbose_name_plural = 'News'
 
+class motors(models.Model):
+    title = models.CharField('Brand', max_length=200)
+    model = models.CharField('Model', max_length=250)
+    photo = models.ImageField(upload_to='photos/%model/')
+    content = models.TextField('About')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Motor'
+        verbose_name_plural = 'Motors'
--- suite/suite/settings.py
+++ suite/suite/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -122,7 +122,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIR = [
     BASE_DIR / "static",
 ]
-
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
